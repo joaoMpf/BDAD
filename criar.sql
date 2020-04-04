0631e1fd0c8f6a1d4e0d5609482d08f63e21c401
up201706978 <up201706978@fe.up.pt>
2020-04-05 00:14:48 +0100

    com sql updated branch master

--- criar.sql
+++ criar.sql
@@ -3,11 +3,12 @@ PRAGMA encoding = "UTF-8";
 DROP TABLE IF EXISTS Person;
 DROP TABLE IF EXISTS Customer;
 DROP TABLE IF EXISTS Driver;
-DROP TABLE IF EXISTS TeamLeader;
+DROP TABLE IF EXISTS Team;
 DROP TABLE IF EXISTS Vehicle;
 DROP TABLE IF EXISTS VehicleDriver;
 DROP TABLE IF EXISTS CreditCard;
-DROP TABLE IF EXISTS Order;
+DROP TABLE IF EXISTS Demand;
+DROP TABLE IF EXISTS Demanded;
 DROP TABLE IF EXISTS PaymentType;
 DROP TABLE IF EXISTS Review;
 DROP TABLE IF EXISTS Rating;
@@ -18,136 +19,185 @@ DROP TABLE IF EXISTS Location;
 DROP TABLE IF EXISTS Invoice;
 DROP TABLE IF EXISTS InvoiceLine;
 CREATE TABLE Person (
-  NIF integer primary key,
-  name text NOTNULL,
-  birth_date integer NOTNULL,
-  email text,
-  phone integer NOTNULL CHECK(length(phone) >= 9),
-  password text NOTNULL
+  NIF INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  birth_date DATE NOT NULL,
+  email TEXT,
+  phone INTEGER NOT NULL CHECK(length(phone) >= 9),
+  password TEXT NOT NULL,
+  CONSTRAINT person_pk PRIMARY KEY (NIF)
 );
 CREATE TABLE Customer (
-  customerNIF integer,
-  FOREIGN KEY(customerNIF) REFERENCES Person(NIF)
+  customerNIF INTEGER,
+  FOREIGN KEY(customerNIF) REFERENCES Person(NIF),
+  CONSTRAINT customer_pk PRIMARY KEY (customerNIF)
 );
 CREATE TABLE Driver (
-  driverNIF integer,
-  FOREIGN KEY(driverNIF) REFERENCES Person(NIF),
-  ss_number integer NOTNULL CHECK(length(ss_number) = 11),
-  start_date text NOTNULL,
-  --rating_average integer DEFAULT NULL --TODO: meter calculos
+  driverNIF INTEGER REFERENCES Person(NIF),
+  start_date DATE NOT NULL,
+  ss_number INTEGER NOT NULL CHECK(length(ss_number) >= 11),
+  rating_average REAL DEFAULT NULL,
+  CONSTRAINT driver_pk PRIMARY KEY (driverNIF)
 );
-CREATE TABLE TeamLeader (
-  --! será que é assim?
-  driver1NIF integer,
-  FOREIGN KEY(driver1NIF) REFERENCES Person(NIF),
-  driver2NIF integer,
-  FOREIGN KEY(driver2NIF) REFERENCES Person(NIF)
+CREATE TABLE Team (
+  driverNIF integer,
+  leaderNIF integer,
+  FOREIGN KEY(driverNIF) REFERENCES Person(NIF),
+  FOREIGN KEY(leaderNIF) REFERENCES Person(NIF),
+  CONSTRAINT team_pk PRIMARY KEY (driverNIF)
 );
 CREATE TABLE Vehicle (
-  license_plate integer NOTNULL CHECK(length(license_plate) >= 6),
-  make text NOTNULL,
-  model text NOTNULL,
-  driverNIF integer,
-  FOREIGN KEY(driverNIF) REFERENCES Driver(driverNIF)
+  license_plate INTEGER NOT NULL CHECK(length(license_plate) >= 6),
+  make TEXT NOT NULL,
+  model TEXT NOT NULL,
+  CONSTRAINT vehicle_pk PRIMARY KEY (license_plate)
 );
 CREATE TABLE VehicleDriver (
-  driver1NIF integer,
-  FOREIGN KEY(driver1NIF) REFERENCES Driver(driverNIF),
-  driver2NIF integer,
-  FOREIGN KEY(driver2NIF) REFERENCES Driver(driverNIF),
-  begin_date text NOTNULL,
-  end_date text NOTNULL,
-  CHECK(begin_date < end_date)
+  driverNIF INTEGER REFERENCES Driver(driverNIF),
+  license_plate INTEGER REFERENCES Vehicle(license_plate),
+  begin_date TEXT NOT NULL,
+  end_date TEXT NOT NULL,
+  CHECK(begin_date < end_date) CONSTRAINT vehicleDriver_pk PRIMARY KEY (driverNIF, license_plate)
 );
 CREATE TABLE CreditCard (
-  number_cc integer NOTNULL CHECK(
+  number_cc INTEGER NOT NULL CHECK(
     length(number_cc) >= 13
     AND length(number_cc) <= 19
   ),
-  cvv integer NOTNULL CHECK(length(cvv) = 3),
-  exp_date text NOTNULL,
-  card_type text NOTNULL,
-  customerNIF integer,
-  FOREIGN KEY(customerNIF) REFERENCES Customer(customerNIF)
-);
-CREATE TABLE Order (
-  orderID integer PRIMARY KEY,
-  date text NOTNULL,
-  price integer NOTNULL CHECK(price > delivery_fee),
-  especification text,
-  delivery_fee integer CHECK(delivery_fee >= 0),
-  customerNIF integer,
+  cvv INTEGER NOT NULL CHECK(length(cvv) = 3),
+  exp_date TEXT NOT NULL,
+  card_type TEXT NOT NULL,
+  customerNIF INTEGER,
   FOREIGN KEY(customerNIF) REFERENCES Customer(customerNIF),
-  driverNIF integer,
-  FOREIGN KEY(driverNIF) REFERENCES Driver(driverNIF),
-  locationID integer,
-  FOREIGN KEY(locationID) REFERENCES Location(locationID),
-  paymentTypeID integer,
-  FOREIGN KEY(paymentTypeID) REFERENCES PaymentType(paymentTypeID),
-  foodID integer,
+  CONSTRAINT credit_card_pk PRIMARY KEY (customerNIF, number_cc)
+);
+CREATE TABLE Demand (
+  demandID INTEGER,
+  date TEXT NOT NULL,
+  specification TEXT,
+  delivery_fee INTEGER NOT NULL CHECK(delivery_fee >= 0),
+  price INTEGER NOT NULL CHECK(price >= delivery_fee),
+  --TODO: meter calculos
+  customerNIF INTEGER REFERENCES Customer(customerNIF),
+  driverNIF INTEGER REFERENCES Driver(driverNIF),
+  locationID INTEGER REFERENCES Location(locationID),
+  paymentTypeID INTEGER REFERENCES PaymentType(paymentTypeID),
+  CONSTRAINT Demand_pk PRIMARY KEY (DemandID)
+);
+CREATE TABLE Demanded (
+  foodID INTEGER,
+  demandID INTEGER,
+  quantity INTEGER NOT NULL,
   FOREIGN KEY(foodID) REFERENCES Food(foodID),
+  FOREIGN KEY(demandID) REFERENCES Demand(demandID),
+  CONSTRAINT Demand_pk PRIMARY KEY (DemandID, foodID)
 );
 CREATE TABLE PaymentType (
-  paymentTypeID integer PRIMARY KEY,
-  type text NOTNULL,
+  paymentTypeID INTEGER PRIMARY KEY,
+  type TEXT NOT NULL
 );
 CREATE TABLE Review (
-  reviewID integer PRIMARY KEY,
-  rating integer CHECK(
+  reviewID INTEGER,
+  --PRIMARY KEY,
+  rating INTEGER CHECK(
     rating > 0
     AND rating <= 5
   ),
-  description text,
-  orderID integer,
-  FOREIGN KEY(orderID) REFERENCES Order(orderID)
+  description TEXT,
+  DemandID INTEGER,
+  FOREIGN KEY(DemandID) REFERENCES Demand(DemandID)
 );
 CREATE TABLE Rating (
-  ratingID integer PRIMARY KEY,
-  rating integer CHECK(
+  ratingID INTEGER PRIMARY KEY,
+  rating INTEGER CHECK(
     rating > 0
     AND rating <= 5
-  )
-);
-CREATE TABLE Food (
-  foodID integer PRIMARY KEY,
-  name text NOTNULL,
-  price integer NOTNULL CHECK(price > 0),
-  restaurantID integer,
+  ),
+  restaurantID INTEGER,
   FOREIGN KEY(restaurantID) REFERENCES Restaurant(restaurantID)
 );
 CREATE TABLE Restaurant (
-  restaurantID integer PRIMARY KEY,
-  name text NOTNULL,
-  NIF integer NOTNULL CHECK(length(NIF) = 9),
-  locationID integer,
-  FOREIGN KEY(locationID) REFERENCES Location(locationID),
-  --rating_average integer,  --TODO: meter calculos
-  restaurantTypeID integer,
+  restaurantID INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  NIF INTEGER NOT NULL CHECK(length(NIF) = 9),
+  locationID INTEGER REFERENCES Location(locationID),
+  rating_average INTEGER,
+  restaurantTypeID INTEGER,
   FOREIGN KEY(restaurantTypeID) REFERENCES RestaurantType(restaurantTypeID)
 );
 CREATE TABLE RestaurantType (
-  restaurantTypeID integer PRIMARY KEY,
-  type text NOTNULL
+  restaurantTypeID INTEGER PRIMARY KEY,
+  type TEXT NOT NULL
+);
+CREATE TABLE Food (
+  foodID INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  price INTEGER NOT NULL CHECK(price > 0),
+  restaurantID INTEGER,
+  FOREIGN KEY(restaurantID) REFERENCES Restaurant(restaurantID)
 );
 CREATE TABLE Location (
-  locationID integer PRIMARY KEY,
-  city text NOTNULL,
-  street_name text NOTNULL,
-  street_number text NOTNULL,
-  postal_code text NOTNULL
+  locationID INTEGER PRIMARY KEY,
+  city TEXT NOT NULL,
+  street_name TEXT NOT NULL,
+  street_number TEXT NOT NULL,
+  postal_code TEXT NOT NULL
 );
 CREATE TABLE Invoice (
-  invoiceID integer PRIMARY KEY,
-  --total integer NOTNULL, --TODO: meter calculos
-  date text NOTNULL,
-  orderID integer,
-  FOREIGN KEY(orderID) REFERENCES Order(orderID)
+  invoiceID INTEGER,
+  total INTEGER NOT NULL,
+  --TODO: meter calculos
+  date TEXT NOT NULL,
+  DemandID INTEGER,
+  FOREIGN KEY(DemandID) REFERENCES Demand(DemandID),
+  CONSTRAINT invoice_pk PRIMARY KEY (InvoiceID)
 );
 CREATE TABLE InvoiceLine (
-  invoice_lineID integer PRIMARY KEY,
-  quantity integer NOTNULL,
-  foodID integer,
-  FOREIGN KEY(foodID) REFERENCES Food(foodID),
-  invoiceID integer,
-  FOREIGN KEY(invoiceID) REFERENCES Invoice(invoiceID)
+  invoice_lineID INTEGER,
+  DemandedID INTEGER,
+  invoiceID INTEGER,
+  FOREIGN KEY(DemandedID) REFERENCES Demanded(DemandID),
+  FOREIGN KEY(invoiceID) REFERENCES Invoice(invoiceID),
+  CONSTRAINT invoice_line_pk PRIMARY KEY (invoice_lineID)
 );
+/*drivers' average rating calculations*/
+CREATE TRIGGER aft_insert_review
+AFTER
+INSERT ON Review BEGIN
+UPDATE Driver
+SET
+  rating_average = (
+    SELECT
+      AVG(Review.rating)
+    FROM Demand
+    JOIN Review
+    WHERE
+      (
+        Demand.driverNIF = Driver.driverNIF
+        AND Demand.demandID = Review.demandID
+      )
+  )
+WHERE
+  driverNIF = Driver.driverNIF;
+END;
+/*restaurants' average rating calculations*/
+CREATE TRIGGER aft_insert_rating
+AFTER
+INSERT ON Rating BEGIN
+UPDATE Restaurant
+SET
+  rating_average = (
+    SELECT
+      AVG(Rating.rating)
+    FROM Demand
+    JOIN Rating
+    WHERE
+      (Rating.restaurantID = Restaurant.restaurantID)
+  )
+WHERE
+  restaurantID = Restaurant.restaurantID;
+END;
+/*Show Driver table*/
+SELECT
+  *
+FROM Driver;
--- povoar.sql
+++ povoar.sql
@@ -1,2 +1,38 @@
 PRAGMA foreign_keys = ON;
 PRAGMA encoding = "UTF-8";
+
+/*----------------------------VALUE INSERTIONS-----------------------------*/
+
+
+INSERT INTO Person VALUES(1,"Joao","01-01-01","joao@email.com",912345678,"1234");
+INSERT INTO Person VALUES(2,"Jose","02-02-02","jose@email.com",923456781,"1234");
+INSERT INTO Person VALUES(3,"Miguel","03-03-03","miguel@email.com",9345678912,"1234");
+
+--SELECT * FROM Person;
+
+INSERT INTO Driver VALUES(1,"01-01-01",12345678901, -1);
+INSERT INTO Driver VALUES(2,"02-02-02",12345678902, -1);
+INSERT INTO Driver VALUES(3,"03-03-03",12345678903, -1);
+
+--SELECT * FROM Driver;
+
+--SELECT name FROM Person JOIN Driver WHERE NIF = driverNIF;
+
+INSERT INTO Demand(DemandID, date, delivery_fee, price, driverNIF) VALUES (1,"01-01-01",1,2,1);
+INSERT INTO Demand(DemandID, date, delivery_fee, price, driverNIF) VALUES (2,"01-01-01",1,2,2);
+INSERT INTO Demand(DemandID, date, delivery_fee, price, driverNIF) VALUES (3,"01-01-01",1,2,3);
+INSERT INTO Demand(DemandID, date, delivery_fee, price, driverNIF) VALUES (4,"01-01-01",1,2,1);
+INSERT INTO Demand(DemandID, date, delivery_fee, price, driverNIF) VALUES (5,"01-01-01",1,2,2);
+INSERT INTO Demand(DemandID, date, delivery_fee, price, driverNIF) VALUES (6,"01-01-01",1,2,3);
+
+--SELECT * FROM Demand;
+
+INSERT INTO Review(reviewID, rating, demandID) VALUES(1,1,1);
+INSERT INTO Review(reviewID, rating, demandID) VALUES(1,5,2);
+INSERT INTO Review(reviewID, rating, demandID) VALUES(2,4,3);
+INSERT INTO Review(reviewID, rating, demandID) VALUES(2,3,4);
+INSERT INTO Review(reviewID, rating, demandID) VALUES(3,5,5);
+INSERT INTO Review(reviewID, rating, demandID) VALUES(3,4,6);
+
+
+
